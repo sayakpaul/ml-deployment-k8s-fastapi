1932849b19b1760522b002456a61cf00b1709984
Chansung Park <deep.diver.csp@gmail.com>
2022-05-03 12:45:09 +0000

    update grpc load test config

--- locust/grpc/locustfile.py
+++ locust/grpc/locustfile.py
@@ -1,49 +1,95 @@
-"""Launch a load-test with Locust for the gRPC API.
+import sys
+import grpc
+import inspect
+import time
+import gevent
 
-Code copied and modified from here: 
-http://docs.locust.io/en/stable/testing-other-systems.html
-"""
+# Libs
+from locust.contrib.fasthttp import FastHttpUser
+from locust import task, events, constant
+from locust.runners import STATE_STOPPING, STATE_STOPPED, STATE_CLEANUP, WorkerRunner
 
-
-import numpy as np
 import tensorflow as tf
-from locust import User, task
 from tensorflow_serving.apis import predict_pb2, prediction_service_pb2_grpc
 
-import grpc
+def stopwatch(func):
+    """To be updated"""
 
-# Patch grpc so that it uses gevent instead of asyncio
-import grpc.experimental.gevent as grpc_gevent
+    def wrapper(*args, **kwargs):
+        """To be updated"""
+        # get task's function name
+        previous_frame = inspect.currentframe().f_back
+        _, _, task_name, _, _ = inspect.getframeinfo(previous_frame)
 
-grpc_gevent.init_gevent()
+        start = time.time()
+        result = None
+        try:
+            result = func(*args, **kwargs)
+        except Exception as e:
+            total = int((time.time() - start) * 1000)
+            events.request_failure.fire(request_type="TYPE",
+                                        name=task_name,
+                                        response_time=total,
+                                        response_length=0,
+                                        exception=e)
+        else:
+            total = int((time.time() - start) * 1000)
+            events.request_success.fire(request_type="TYPE",
+                                        name=task_name,
+                                        response_time=total,
+                                        response_length=0)
+        return result
 
+    return wrapper
 
-class ImgClssificationUser(User):
+class GRPCMyLocust(FastHttpUser):
+    host = 'http://104.155.184.208'
+    wait_time = constant(1)
+
     def __init__(self, environment):
         super().__init__(environment)
-        self.channel = grpc.insecure_channel("34.135.245.34:8500")
+        self.channel = grpc.insecure_channel("104.155.184.208:8500")
         self.stub = prediction_service_pb2_grpc.PredictionServiceStub(
             self.channel
         )
         self.request = predict_pb2.PredictRequest()
         self.request.model_spec.name = "resnet"
-        self.request.model_spec.signature_name = "serving_default"
+        self.request.model_spec.signature_name = "serving_default"    
 
-    def prepare_img(self):
-        image = tf.image.decode_jpeg(tf.io.read_file("../cat.jpg"))
-        image = tf.image.resize(image, (224, 224))[None, ...]
-        return image
+    def on_start(self):
+        """ on_start is called when a Locust start before any task is scheduled """
+        pass
+
+    def on_stop(self):
+        """ on_stop is called when the TaskSet is stopping """
+        pass
 
     @task
-    def predict(self):
-        sample_image = self.prepare_img()
+    @stopwatch
+    def grpc_client_task(self):
+        """To be updated"""
+        try:
+            sample_image = tf.image.decode_jpeg(tf.io.read_file("./cat_224x224.jpg"))
 
-        self.request.inputs["image_input"].CopyFrom(
-            tf.make_tensor_proto(sample_image)
-        )
-        preds = self.stub.Predict(self.request, 30.0)
+            self.request.inputs["image_input"].CopyFrom(
+                tf.make_tensor_proto(sample_image)
+            )
+            preds = self.stub.Predict(self.request, 30.0)                
 
-        # Uncomment to debug.
-        # preds = preds.outputs["resnet50"].float_val
-        # preds = np.array(preds).reshape(1, -1)
-        # print("Prediction class: {}".format(np.argmax(preds, axis=-1)))
+        except (KeyboardInterrupt, SystemExit):
+            sys.exit(0)
+
+# Stopping the locust if a threshold (in this case the fail ratio) is exceeded
+def checker(environment):
+    while not environment.runner.state in [STATE_STOPPING, STATE_STOPPED, STATE_CLEANUP]:
+        time.sleep(1)
+        if environment.runner.stats.total.fail_ratio > 0.2:
+            print(f"fail ratio was {environment.runner.stats.total.fail_ratio}, quitting")
+            environment.runner.quit()
+            return
+
+
+@events.init.add_listener
+def on_locust_init(environment, **_kwargs):
+    if not isinstance(environment.runner, WorkerRunner):
+        gevent.spawn(checker, environment)
